92a450a062860a1ad0c2f8270ed15da34b1636d9
Pankajjr12 <67278679+Pankajjr12@users.noreply.github.com>
2020-10-14 21:44:33 +0530

    Kotlin_Programs

---
+++ Kotlin_programs/string.kt
@@ -0,0 +1,8 @@
+fun main()
+{
+ var str="p a n k a j"
+    str=str.replace("\\s".toRegex(),"------")
+    println(str)
+    var str1="Pankaj Kumar"
+
+}
---
+++ Kotlin_programs/string2.kt
@@ -0,0 +1,6 @@
+fun main()
+{
+   var string="Hello Kotlin"
+    println("Orginal String is $string")
+    println("Substring "+" "+string.subSequence(6,12))
+}
